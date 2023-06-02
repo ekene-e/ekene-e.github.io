ef13414d4770512126194f9956b18849874571ca
Ekene Ezeunala <96993777+ekene-e@users.noreply.github.com>
2023-06-02 18:27:37 -0400

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,31 @@
+<footer class="layout-md mt-20 text-lg flex flex-col">
+  <div class="row">
+    <span>Twitter</span>
+    <hr />
+    <a class="link" href="https://twitter.com/ekene">@ekene1</a>
+  </div>
+  <div class="row">
+    <span>GitHub</span>
+    <hr />
+    <a class="link" href="https://github.com/ekene-e">@ekene-e</a>
+  </div>
+  <div class="row">
+    <span>Email</span>
+    <hr />
+    <a class="link" href="mailto:ezeunalaekene@gmail.com">ezeunalaekene@gmail.com</a>
+  </div>
+</footer>
 
+<style lang="postcss">
+  .row {
+    @apply flex items-center space-x-4;
+  }
+
+  .row span {
+    @apply text-neutral-500;
+  }
+
+  .row hr {
+    @apply w-full mt-0.5 border-neutral-300 border-dotted;
+  }
+</style>
